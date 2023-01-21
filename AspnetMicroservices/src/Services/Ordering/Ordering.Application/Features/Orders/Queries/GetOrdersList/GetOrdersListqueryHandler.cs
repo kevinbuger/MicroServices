54436c14bdd616081efca5c7fa2a15dc753950d3
k.deyjoori <k.deyjoori@112-1296C>
2023-01-21 11:33:33 +0330

    Add Clean Architecture

---
+++ AspnetMicroservices/src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListqueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListqueryHandler: IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrdersListqueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<OrdersVm>> Handle(GetOrdersListQuery reqeust, CancellationToken cancellationToken)
+        {
+            var orderList = await _orderRepository.GetOrderByUserName(reqeust.UserName);
+            return _mapper.Map<List<OrdersVm>>(orderList);
+        }
+    }
+}
