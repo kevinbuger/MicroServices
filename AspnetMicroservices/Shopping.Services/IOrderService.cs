c9e18ab0a244ff1375f4d350528e0aa86fc99c8f
k.deyjoori <k.deyjoori@112-1296C>
2023-01-30 15:32:38 +0330

    Add  Aggregator

---
+++ AspnetMicroservices/Shopping.Services/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using Shopping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Services
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName);
+    }
+}
---
+++ AspnetMicroservices/Shopping.Services/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using Shopping.Models;
+using Shopping.Services.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly HttpClient _client;
+
+        public OrderService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+        {
+            var response = await _client.GetAsync($"/api/v1/Order/{userName}");
+            return await response.ReadContentAs<List<OrderResponseModel>>();
+        }
+    }
+}
