ee65868bd8b7f3cb17c65f7a64aebb4d50ea1778
k.deyjoori <k.deyjoori@112-1296C>
2023-02-01 13:45:31 +0330

    Add Web App

---
+++ AspnetMicroservices/src/WebApps/AspnetRunBasics/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using AspnetRunBasics.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddRazorPages();
+
+builder.Services.AddHttpClient<ICatalogService, CatalogService>(c => c.BaseAddress = new Uri(builder.Configuration.GetSection("ApiSettings:GatewayAddress").Value));
+builder.Services.AddHttpClient<IBasketService, BasketService>(c => c.BaseAddress = new Uri(builder.Configuration.GetSection("ApiSettings:GatewayAddress").Value));
+builder.Services.AddHttpClient<IOrderService, OrderService>(c => c.BaseAddress = new Uri(builder.Configuration.GetSection("ApiSettings:GatewayAddress").Value));
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+app.MapRazorPages();
+
+app.Run();
